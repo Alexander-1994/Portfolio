aa50efc842d40a647c2987f3190760a6fc704405
Alexander <kisliy.doma@gmail.com>
2022-04-02 22:59:07 +0800

    Desctop

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-// Создание интерактивности нашему меню
-const hamburgerElem = document.querySelector('.hamburger'),     /* константы */
+// Добавление интерактивности нашему меню
+const hamburgerElem = document.querySelector('.hamburger'),                   /* константы. В начале нужно получить элементы, с которыми будет взаимодействие */
     menuElem = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close'),
     overlayElem = document.querySelector('.menu__overlay');
@@ -10,7 +10,23 @@ hamburgerElem.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menuElem.classList.remove('active')
 });
-overlayElem.addEventListener('click', () => {                   /* +я еще добавил, что при клике на "затемнение" рядом с меню, оно так же скроется */
+overlayElem.addEventListener('click', () => {                                 /* +я еще добавил, что при клике на "затемнение" рядом с меню, оно так же скроется */
     menuElem.classList.remove('active')
 });
-// .querySelector-команда, позволяющая получать элемент по селектору; .addEventListener-команда, отслеживающая(все клики в данном случае); .classList.add-команда, добавляющая элементу определенный класс; .classList.remove-команда, убирающая у элемента определенный класс.
+
+// Добавление интерактивности индикаторам
+const counters = document.querySelectorAll('.skills__ratings-indicator'),
+    lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+
+
+// .querySelector()-команда, позволяющая получать элемент по селектору;
+// .addEventListener('click', () => -команда, отслеживающая все клики (в данном случае) и вызывающая после этого опред. функцию;
+// .classList.add()-команда, добавляющая элементу определенный класс;
+// .classList.remove()-команда, убирающая у элемента определенный класс;
+// .forEach((item, i) => -метод, который проходится по "хранилищу" опред. элементов и что-то делает с каждым из них (вызов функции);
+// .innerHTML-команда, позволяющая вытащить содержимое опред. элемента;
